787ebbbb4c95d0b5534d058fca3d16ea9724a9fe
Gabrielcifu <gabomate27@gmail.com>
2025-05-25 18:42:06 -0500

    codigoTerminadoHeren

--- src/codHerencia.java
+++ src/codHerencia.java
@@ -1,2 +1,22 @@
 public class codHerencia {
+         protected String reino;
+         protected String filo;
+         protected String clase;
+         protected String orden;
+         protected String familia;
+         protected String genero;
+         protected String especie;
+
+         public codHerencia(String reino, String filo, String clase, String orden,String familia, String genero, String especie){
+             this.reino=reino;
+             this.filo=filo;
+             this.clase=clase;
+             this.orden=orden;
+             this.familia=familia;
+             this.genero=genero;
+             this.especie=especie;
+         }
 }
+
+
+
